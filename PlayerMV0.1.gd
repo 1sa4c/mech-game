3cd0285f65504a8feef77952f8688e82972f7bda
1sa4c <50160939+1sa4c@users.noreply.github.com>
2023-12-30 19:52:25 -0300

    Smoother player movement

--- PlayerMV0.1.gd
+++ PlayerMV0.1.gd
@@ -1,31 +1,40 @@
 extends CharacterBody3D
 
+var speed = 7
+const ACCEL_DEFAULT = 7
+const ACCEL_AIR = 1
+@onready var accel = ACCEL_DEFAULT
+var gravity = 9.8
+var jump = 5
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+var snap
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
-
+var gravity_vec = Vector3()
+var movement = Vector3()
+		
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	#get keyboard input
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	
+	#jumping and gravity
+	if is_on_floor():
+		snap = -get_floor_normal()
+		accel = ACCEL_DEFAULT
+		gravity_vec = Vector3.ZERO
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		snap = Vector3.DOWN
+		accel = ACCEL_AIR
+		gravity_vec += Vector3.DOWN * gravity * delta
+		
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		snap = Vector3.ZERO
+		gravity_vec = Vector3.UP * jump
+	
+	#make it move
+	velocity = velocity.lerp(direction * speed, accel * delta)
+	movement = velocity + gravity_vec
+	floor_snap_length = len(snap); 
 	move_and_slide()
+	
+	
