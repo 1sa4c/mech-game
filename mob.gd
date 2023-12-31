a398c87959480eb520caa711231be74cf569295b
1sa4c <50160939+1sa4c@users.noreply.github.com>
2023-12-30 22:24:08 -0300

    feat: new enemy that follows player

---
+++ mob.gd
@@ -0,0 +1,19 @@
+extends CharacterBody3D
+
+var speed = 2;
+var player_chase = false;
+@onready var player = get_node("/root/BSC/player")
+
+func _physics_process(delta):
+	if player_chase:
+		position += (player.position - position).normalized() * speed * delta
+		move_and_collide(Vector3(0, 0, 0)) 
+
+func _on_detection_area_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body == player:
+		player_chase = true;
+
+
+func _on_detection_area_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+	if body == player:
+		player_chase = false;
