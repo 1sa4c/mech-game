67e380523693462f123fd1729b3e3fbabf4901ec
John Druett <johnldruett@gmail.com>
2023-12-30 21:06:49 -0300

    Fixed merges
    note for future self, remember to separate the branches!

---
+++ Game Components/interaction_area.gd
@@ -0,0 +1,5 @@
+class_name interactable extends Area3D
+
+@export var int_default = ("none")
+@export var int_type = ("none")
+@export var int_active = ("none")
--- PlayerMV0.1.gd
+++ PlayerMV0.1.gd
@@ -11,8 +11,18 @@ var snap
 
 var gravity_vec = Vector3()
 var movement = Vector3()
-		
+	
+@onready var interaction_storage = []
+@onready var int_visual = $InteractionComps/InteracionLabel
+
+func _ready():
+	update_interactions()
+	
 func _physics_process(delta):
+	#interaction input
+	if Input.is_action_just_pressed("Interact"):
+		execute_interaction()
+	
 	#get keyboard input
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -34,7 +44,32 @@ func _physics_process(delta):
 	#make it move
 	velocity = velocity.lerp(direction * speed, accel * delta)
 	movement = velocity + gravity_vec
-	floor_snap_length = len(snap); 
+#	floor_snap_length = len(snap); 
 	move_and_slide()
 	
+	
+
+#area interaction enter n exit detection
+func _on_interaction_area_area_entered(area):
+	interaction_storage.insert(0, area)
+	update_interactions()
+
+func _on_interaction_area_area_exited(area):
+	interaction_storage.erase(area)
+	update_interactions()
+	
+#area interaction update
+func update_interactions():
+	if interaction_storage:
+		int_visual.text = interaction_storage[0].int_default
+	else:
+		int_visual.text = ""
+	pass
+	
+#user input interaction
+func execute_interaction():
+	if interaction_storage:
+		var current_interaction = interaction_storage[0]
+		match current_interaction.int_type:
+			"print_text" : print(current_interaction.int_active)
 	
